1651b89b3837b266f0615b294e171c0313801bef
Dikra <elmouttakidikra@gmail.com>
2019-05-01 16:48:40 +0200

    projet3

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice1EnfantComponent implements OnInit {
 
+  @Input() public compteur : number = 0;
+
+  @Output() public incremente:EventEmitter<number> = new EventEmitter();
+  @Output() public decremente:EventEmitter<number> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  incrementeCompteur() {
+    this.incremente.emit(this.compteur++);
+  }
+
+  decrementeCompteur() {
+    this.decremente.emit(this.compteur--);
+  }
 }
--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -6,11 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1.component.css']
 })
 export class Exercice1Component implements OnInit {
-  public compteur: number = 0; 
+  public compteur: number = 0;
+  public result: number = 0;
+ 
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  incrementeC() {
+    this.result = this.compteur++;
+}
+decrementeC() {
+    this.result= this.compteur--;
+}
 }
